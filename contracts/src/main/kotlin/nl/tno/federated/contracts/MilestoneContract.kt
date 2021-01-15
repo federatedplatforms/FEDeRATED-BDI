fa904731f0b812f6c1898e2f9508e68dea4bfd98
Andrea d'Auria <andrea.dauria@tno.nl>
2021-01-15 17:47:46 +0100

    Integration of DigitalTwinState in MileStone flow and contract
    The intended behavior is the following:
    When creating an Arrival MileStone it is expected for all the related DT to be found as previously created states in the vault, and to be used as input states.
    In other words:
    - input: all the DT states related to a milestone;
    - output: 1 milestone state and all the DT states with updated information;
    This design choice has still to be confirmed.
    
    MilestoneFlows.kt and MilestoneContract.kt have been edited to account for the aforementioned behavior.
    
    In MilestoneFlowTests.kt two new basic tests to verify that the code runs have been written, but old tests still need to be updated according to the new behavior of the flow.

--- contracts/src/main/kotlin/nl/tno/federated/contracts/MilestoneContract.kt
+++ contracts/src/main/kotlin/nl/tno/federated/contracts/MilestoneContract.kt
@@ -1,13 +1,10 @@
 package nl.tno.federated.contracts
 
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.Requirements.using
-import net.corda.core.contracts.requireSingleCommand
-import net.corda.core.contracts.requireThat
+import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 import nl.tno.federated.states.MilestoneState
 import nl.tno.federated.states.MilestoneType
+import nl.tno.federated.states.DigitalTwinState
 
 // ************
 // * Contract *
@@ -25,16 +22,31 @@ class MilestoneContract : Contract {
         val inputStates = tx.inputStates
         val outputStates = tx.outputStates
         requireThat {
-            "An single output state must be passed." using (outputStates.size == 1)
-            "The output must be a milestone state" using (outputStates.single() is MilestoneState)
+            "An single MilestoneState output must be passed." using (outputStates.filter { it is MilestoneState }.size == 1)
         }
-        val milestoneState = outputStates.single() as MilestoneState
-        require(milestoneState.digitalTwins.isNotEmpty()) { "Digital twins must be linked." }
+
+        val milestoneState = outputStates.filter { it is MilestoneState }.single() as MilestoneState
+
+        // Building the list of ID of Digital Twins passed as input states
+        val idOfDTinput : MutableList<UniqueIdentifier> = mutableListOf()
+
+        for(state in inputStates){
+            when(state) {
+                is DigitalTwinState -> {
+                    val dtState = state as DigitalTwinState
+                    idOfDTinput.add(dtState.linearId)
+                }
+            }
+        }
+        requireThat {
+            "Digital twins must be linked" using (milestoneState.digitalTwins.isNotEmpty())
+            "Digital twins must exist" using (idOfDTinput.containsAll(milestoneState.digitalTwins))
+        }
 
         when(command.value) {
             is Commands.Arrive -> {
                 requireThat {
-                    "Arrival is the first step in a process. No input state may be passed." using (inputStates.isEmpty())
+                    "Arrival is the first step in a process. No milestone input state may be passed." using (inputStates.filter { it is MilestoneState }.isEmpty())
                     "An arrival output state must be passed." using (milestoneState.type == MilestoneType.ARRIVE)
                     "A counterparty must exist, sender shouldn't transact with itself alone." using (milestoneState.participants.count() > 1)
                 }
