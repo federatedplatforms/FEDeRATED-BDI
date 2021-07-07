b3afaf19fe68421a6cea4d952218aa653c621c15
Andrea d'Auria <andrea.dauria@tno.nl>
2021-07-07 19:24:44 +0200

    Added first basic constraints

--- contracts/src/main/kotlin/nl/tno/federated/contracts/EventNewContract.kt
+++ contracts/src/main/kotlin/nl/tno/federated/contracts/EventNewContract.kt
@@ -17,7 +17,22 @@ class EventNewContract : Contract {
     // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
     // does not throw an exception.
     override fun verify(tx: LedgerTransaction) {
+
+        val command = tx.commands.requireSingleCommand<Commands>()
+
         val outputStates = tx.outputStates
+
+        val eventState = outputStates.filterIsInstance<EventNewState>().single()
+
+        requireThat{
+            "Goods and locations cannot be linked together" using (!(eventState.goods.isNotEmpty() && eventState.location.isNotEmpty()))
+            "There can be one location only" using (eventState.location.size <= 1)
+            "There can be one good only" using (eventState.goods.size <= 1)
+            "There can be one transport mean only" using (eventState.transportMean.size <= 1)
+            // there can be one "other" only?
+            "There must be at least a connection" using (eventState.goods.size + eventState.location.size + eventState.transportMean.size + eventState.otherDigitalTwins.size >= 2)
+            // requirements about eCMR?
+        }
 
     }
 
