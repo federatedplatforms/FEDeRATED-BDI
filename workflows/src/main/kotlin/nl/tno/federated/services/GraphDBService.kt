a31755bcd5491b4267ece57d934ae527e8bdb11f
Andrea d'Auria <andrea.dauria@tno.nl>
2021-09-23 17:11:28 +0200

    Small fix
    - parser made non-private
    - fix in the splitting for timestamp extraction

--- workflows/src/main/kotlin/nl/tno/federated/services/GraphDBService.kt
+++ workflows/src/main/kotlin/nl/tno/federated/services/GraphDBService.kt
@@ -114,7 +114,7 @@ object GraphDBService {
         }
     }
 
-    private fun parseRDFtoEvent(rdfFullData: String ) : Event {
+    fun parseRDFtoEvent(rdfFullData: String ) : Event {
 
         val goods = emptyList<UUID>().toMutableList()
         val transportMean = emptyList<UUID>().toMutableList()
@@ -148,7 +148,9 @@ object GraphDBService {
                 var type = EventType.PLANNED // Default type if not found
                 for(lineSecondScan in lines) {
                     if(lineSecondScan.contains("Event:hasDateTimeType")) {
-                        val stringType = lineSecondScan.split(" ")[1].split(":",";")[1]
+                        val stringType = lineSecondScan
+                                .substringAfter("Event:hasDateTimeType ")
+                                .split(":",";")[1]
                         when(stringType.toLowerCase()) {
                             "actual" -> type = EventType.ACTUAL
                             "estimated" -> type = EventType.ESTIMATED
