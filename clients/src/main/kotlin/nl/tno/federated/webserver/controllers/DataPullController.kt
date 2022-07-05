79b72ed20476a3a8bad17dae25a8ae5e43c86772
Andrea d'Auria <andrea.dauria@tno.nl>
2022-07-05 14:18:14 +0200

    Generic L1 functions in controllers are now referenced

--- clients/src/main/kotlin/nl/tno/federated/webserver/controllers/DataPullController.kt
+++ clients/src/main/kotlin/nl/tno/federated/webserver/controllers/DataPullController.kt
@@ -7,6 +7,8 @@ import net.corda.core.node.services.vault.QueryCriteria
 import nl.tno.federated.flows.*
 import nl.tno.federated.states.Event
 import nl.tno.federated.states.EventState
+import nl.tno.federated.webserver.L1Services.extractAccessTokenFromHeader
+import nl.tno.federated.webserver.L1Services.userIsAuthorized
 import nl.tno.federated.webserver.NodeRPCConnection
 import nl.tno.federated.webserver.dtos.NewEvent
 import nl.tno.federated.webserver.dtos.QueryAndOrganization
@@ -37,7 +39,7 @@ class DataPullController(rpc: NodeRPCConnection) {
     private val proxy = rpc.proxy
 
     @ApiOperation(value = "Request data and run a SPARQL query on another node")
-    @GetMapping(value = ["/datapull/"])
+    @GetMapping(value = ["/pull/"])
     private fun datapull(@RequestBody queryInfo : QueryAndOrganization, @RequestHeader("Authorization") authorizationHeader: String): ResponseEntity<String> {
         val accessToken = extractAccessTokenFromHeader(authorizationHeader)
 
@@ -54,108 +56,4 @@ class DataPullController(rpc: NodeRPCConnection) {
             return ResponseEntity("Something went wrong: $e", HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-
-    private fun isLettersOrDigits(chars: String): Boolean {
-        return chars.none { it !in 'A'..'Z' && it !in 'a'..'z' && it !in '0'..'9' && it != '-' && it != '_' }
-    }
-
-    private fun isJWTFormatValid(token: String?) : Boolean {
-        if(token == null) return false
-        val splitToken = token.split(".")
-        return (splitToken.size == 3 && splitToken.all { isLettersOrDigits(it) } )
-    }
-
-    private fun retrieveUrlBody(url: URL, requestMethod: RequestMethod, body: String = ""): String {
-        val con = url.openConnection() as HttpURLConnection
-        con.requestMethod = requestMethod.toString()
-        con.connectTimeout = 5000
-        con.readTimeout = 5000
-        con.setRequestProperty("Content-Type", "application/json")
-        con.setRequestProperty("Accept", "application/json")
-
-        if (body.isNotBlank()) {
-            con.doOutput = true
-            con.outputStream.use { os ->
-                val input: ByteArray = body.toByteArray(StandardCharsets.UTF_8)
-                os.write(input, 0, input.size)
-            }
-        }
-
-        if (con.responseCode in 200..299) {
-            con.inputStream.bufferedReader().use {
-                return it.readText()
-            }
-        }
-        else {
-            con.errorStream.bufferedReader().use {
-                return it.readText()
-            }
-        }
-    }
-
-    enum class RequestMethod {
-        GET, POST
-    }
-
-    private fun extractAuthorizationResult(authorizationResult: String): Boolean {
-        val polishedString = authorizationResult.split('\n')
-        for(line in polishedString) {
-            if(line.contains("success") && line.contains("true")) return true
-        }
-        return false
-    }
-
-    private fun userIsAuthorized(token: String) : Boolean {
-        val salt = "because we like best practices"
-        val hashedBackdoor = hashSHA256(token+salt)
-
-        return hashedBackdoor == "E812D42535F643547727FA98B9B1DE56C81F7F3100004684C42DFD5C5014AF5E" || validateToken(token)
-    }
-
-    private fun validateToken(@RequestBody token: String) : Boolean {
-        if(!isJWTFormatValid(token)) return false
-
-        val url = URL("http://federated.sensorlab.tno.nl:1003/validate/token")
-        val body = """
-            {
-                "access_token": "Bearer $token"
-            }
-            """.trimIndent()
-        val result = retrieveUrlBody(url,
-                DataPullController.RequestMethod.POST,
-                body
-        )
-
-        return extractAuthorizationResult(result)
-    }
-
-    private fun extractAccessTokenFromHeader(authorizationHeader: String): String {
-        val authorizationHeaderWords = authorizationHeader.split(" ")
-        if (authorizationHeaderWords.isEmpty() || authorizationHeaderWords.first() != "Bearer")
-            throw AuthenticationException("Authorization header is malformed")
-
-        else return authorizationHeaderWords[1]
-    }
-
-    // Source: https://gist.github.com/lovubuntu/164b6b9021f5ba54cefc67f60f7a1a25
-    private fun hashSHA256(string: String): String {
-        val bytes = string.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        return digest.fold("", { str, it -> str + "%02x".format(it) }).toUpperCase()
-    }
-
-    private fun eventStatesToEventMap(eventStates: List<EventState>) =
-        eventStates.associate {
-            it.linearId.id to Event(
-                it.goods,
-                it.transportMean,
-                it.location,
-                it.otherDigitalTwins,
-                it.timestamps,
-                it.ecmruri,
-                it.milestone,
-                it.fullEvent
-            )
-        }
 }
