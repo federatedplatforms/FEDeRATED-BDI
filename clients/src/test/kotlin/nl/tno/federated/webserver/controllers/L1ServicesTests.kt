e91fcdcc44dc41060d316dbd5b2409d86e782496
Erik de Graaf <Erik.deGraaf@tno.nl>
2022-09-14 09:27:33 +0200

    ignore identity token tests

--- clients/src/test/kotlin/nl/tno/federated/webserver/controllers/L1ServicesTests.kt
+++ clients/src/test/kotlin/nl/tno/federated/webserver/controllers/L1ServicesTests.kt
@@ -1,16 +1,11 @@
 package nl.tno.federated.webserver.controllers
 
-import nl.tno.federated.states.EventType
-import nl.tno.federated.states.Milestone
-import nl.tno.federated.webserver.L1Services.getIBMIdentityToken // cannot import if function is private
+import nl.tno.federated.webserver.L1Services.getIBMIdentityToken
 import nl.tno.federated.webserver.L1Services.getSolutionToken
 import org.junit.After
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
-import java.io.File
-import java.util.*
-import kotlin.test.assertEquals
 
 
 class L1ServicesTests {
@@ -23,7 +18,7 @@ class L1ServicesTests {
     fun tearDown() {
     }
 
-//    @Ignore
+    @Ignore
     @Test // Not a real test, here just to inspect what getIBMIdentityToken() is returning
     fun `Get IBM Identity Token`() {
 //         Make getIBMIdentityToken() internal if you want to test it singularly.
