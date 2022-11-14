f896b3cf3b56e51cb08a468fc4d015a350aef03b
Erik de Graaf <Erik.deGraaf@tno.nl>
2022-11-14 15:24:21 +0100

    Corda upgrade and removal of old deployServerNodes gradle config

--- contracts/src/main/kotlin/nl/tno/federated/states/DataPullState.kt
+++ contracts/src/main/kotlin/nl/tno/federated/states/DataPullState.kt
@@ -4,13 +4,7 @@ import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.AbstractParty
-import net.corda.core.schemas.MappedSchema
-import net.corda.core.schemas.PersistentState
-import net.corda.core.schemas.QueryableState
-import net.corda.core.serialization.CordaSerializable
 import nl.tno.federated.contracts.DataPullContract
-import nl.tno.federated.contracts.EventContract
-import java.util.*
 
 // *********
 // * State *
