befe234254cc4773d1af9628b1dd8cbe79274fb5
Stephan Oudmaijer <stephan.oudmaijer@tno.nl>
2022-12-01 12:00:00 +0100

    unused

--- api/src/main/kotlin/nl/tno/federated/api/dtos/PostEventDTO.kt
+++
@@ -1,9 +0,0 @@
-package nl.tno.federated.api.dtos
-
-import net.corda.core.serialization.CordaSerializable
-
-@CordaSerializable
-data class NewEvent(
-        val fullEvent: String,
-        val countriesInvolved : Set<String>
-)
