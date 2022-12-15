0aa914d4f0cb2584054335f71c8b049bac5be892
Theodor-Christian Chirvasuta <theodor-christian.chirvasuta@tno.nl>
2022-12-15 16:58:14 +0100

    Fixed location parser, also with a test (credits for the test: Andrea)

--- corda/workflows/src/main/kotlin/nl/tno/federated/corda/services/graphdb/GraphDBEventConverter.kt
+++ corda/workflows/src/main/kotlin/nl/tno/federated/corda/services/graphdb/GraphDBEventConverter.kt
@@ -32,7 +32,10 @@ object GraphDBEventConverter {
     fun parseRDFtoCountries(rdfFullData: String): List<String> {
         val model = parseRDFToModel(rdfFullData)
         val eventIDs = eventIdsFromModel(model)
-        return parseModelToMapEventsCountry(model, eventIDs)[eventIDs[0]]!!
+        val eventIdentifier = eventIDs.single().substringAfter("-")
+        val mappedEventsToCountries = parseModelToMapEventsCountry(model, eventIDs)
+        return mappedEventsToCountries[eventIdentifier]!!
+
     }
 
     // for quicker tests in GraphDBTripTests
--- corda/workflows/src/test/kotlin/nl/tno/federated/corda/services/graphdb/GraphDBEventConverterTest.kt
+++ corda/workflows/src/test/kotlin/nl/tno/federated/corda/services/graphdb/GraphDBEventConverterTest.kt
@@ -276,4 +276,58 @@ class GraphDBEventConverterTest {
         assertEquals(1, parsedEvent.otherDigitalTwins.size)
         assertEquals(UUID.fromString("dce1774a-b2a1-338e-bd56-1902c57f836f"), parsedEvent.otherDigitalTwins.single())
     }
+
+    @Test
+    fun `parse country`() {
+        val testRdfEvent = """
+        @base <http://example.com/base/> . 
+        @prefix owl: <http://www.w3.org/2002/07/owl#> . 
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix data: <http://example.com/base#> .
+        @prefix ex: <http://example.com/base#> . 
+        @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+        @prefix time: <http://www.w3.org/2006/time#> . 
+        @prefix data: <http://example.com/base#> .
+        @prefix ex: <http://example.com/base#> . 
+        @prefix Event: <https://ontology.tno.nl/logistics/federated/Event#> . 
+        @prefix pi: <https://ontology.tno.nl/logistics/federated/PhysicalInfrastructure#> .
+        @prefix businessService: <https://ontology.tno.nl/logistics/federated/BusinessService#> .
+        @prefix dt: <https://ontology.tno.nl/logistics/federated/DigitalTwin#> .
+        @prefix classifications: <https://ontology.tno.nl/logistics/federated/Classifications#> .
+        
+                ex:LegalPerson-Qjvtcz a businessService:LegalPerson, owl:NamedIndividual, businessService:PrivateEnterprise;
+                  businessService:actorName "Qjvtcz" .
+                    
+                ex:Equipment-3d623de9-aa2c-4f76-b4e8-6685a66d63e3 a dt:Equipment, owl:NamedIndividual;
+                  rdfs:label "TNO-test092022" .
+                    
+                ex:businessTransaction-dace89b9-b771-4de6-86ec-4d0dc702a348 a businessService:Consignment, owl:NamedIndividual;
+                  businessService:consignmentCreationTime "2022-01-01T00:01:00"^^xsd:dateTime;
+                  businessService:involvedActor ex:LegalPerson-Qjvtcz .
+                    
+                ex:PhysicalInfrastructure-ZULZW a pi:Location, owl:NamedIndividual;
+                    pi:cityName "Maastricht" ;
+                    pi:countryName "NL" .
+                 
+                ex:dt-cc145613-3825-4de2-84d4-475ac295f498 a dt:TransportMeans, owl:NamedIndividual, dt:Vessel;
+                  rdfs:label "Vessel";
+                  dt:hasVIN "1879236";
+                  dt:hasTransportMeansID "1879236" .
+                     
+                ex:Event-a2f4c4fd-f9ac-40f1-ac9e-cd08e1b352fb a Event:Event, owl:NamedIndividual;
+                  Event:hasTimestamp "2018-07-28T00:08:04Z"^^xsd:dateTime;
+                  Event:hasDateTimeType Event:Planned;
+                  Event:involvesDigitalTwin ex:dt-cc145613-3825-4de2-84d4-475ac295f498, ex:Equipment-3d623de9-aa2c-4f76-b4e8-6685a66d63e3;
+                  Event:involvesBusinessTransaction ex:businessTransaction-dace89b9-b771-4de6-86ec-4d0dc702a348;
+                  Event:involvesPhysicalInfrastructure ex:PhysicalInfrastructure-ZULZW;
+                  Event:hasMilestone Event:Start;
+                  Event:hasSubmissionTimestamp "2018-06-28T00:08:04Z"^^xsd:dateTime .
+        """.trimIndent()
+
+        val parsedEvent = eventConverter.parseRDFToEvents(testRdfEvent).single()
+        val parsedCountry = eventConverter.parseRDFtoCountries(testRdfEvent)
+
+        assertEquals("NL", parsedCountry.single())
+    }
 }
