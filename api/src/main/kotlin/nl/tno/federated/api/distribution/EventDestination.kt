fc3325fcd61337fb25fd04e1924b323b80601113
Stephan Oudmaijer <stephan.oudmaijer@tno.nl>
2022-12-20 10:03:40 +0100

    polish

--- api/src/main/kotlin/nl/tno/federated/api/distribution/EventDestination.kt
+++ api/src/main/kotlin/nl/tno/federated/api/distribution/EventDestination.kt
@@ -2,5 +2,4 @@ package nl.tno.federated.api.distribution
 
 import net.corda.core.identity.CordaX500Name
 
-class EventDestination(val cordaX500Name: CordaX500Name) {
-}
+class EventDestination(val cordaX500Name: CordaX500Name)
