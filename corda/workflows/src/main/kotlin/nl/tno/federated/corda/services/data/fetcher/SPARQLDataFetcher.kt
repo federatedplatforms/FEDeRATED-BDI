0188858e57f692f5093e242dd542184e488a3b88
Stephan Oudmaijer <stephan.oudmaijer@tno.nl>
2023-06-15 13:33:55 +0200

    Make the sparql data fetcher URL configurable.

--- corda/workflows/src/main/kotlin/nl/tno/federated/corda/services/data/fetcher/SPARQLDataFetcher.kt
+++ corda/workflows/src/main/kotlin/nl/tno/federated/corda/services/data/fetcher/SPARQLDataFetcher.kt
@@ -3,12 +3,18 @@ package nl.tno.federated.corda.services.data.fetcher
 import org.eclipse.rdf4j.query.TupleQuery
 import org.eclipse.rdf4j.query.resultio.sparqljson.SPARQLResultsJSONWriter
 import org.eclipse.rdf4j.repository.sparql.SPARQLRepository
+import org.slf4j.LoggerFactory
+import java.io.InputStream
 import java.io.StringWriter
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
 
 class SPARQLDataFetcher : DataFetcher {
 
-    private val url = "http://localhost:7200/repositories/default"
-    private val repository = SPARQLRepository(url)
+    private val repository by lazy { SPARQLRepository(properties.getProperty("graphdb.sparql.url")) }
+    private val log = LoggerFactory.getLogger(SPARQLDataFetcher::class.java)
+    private val propertiesFileName = "datafetcher.properties"
 
     override fun fetch(input: String): String? {
         return executeSPARQL(input)
@@ -23,5 +29,33 @@ class SPARQLDataFetcher : DataFetcher {
             }
             sw.toString()
         }
+    }
+    
+    private val properties: Properties by lazy {
+        getInputStreamFromClassPathResource(propertiesFileName).use {
+            if (it == null) log.warn("${propertiesFileName} could not be found!")
+            val properties = Properties()
+            properties.load(it)
+
+            with(System.getProperties()) {
+                getProperty("graphdb.sparql.url")?.run {
+                    log.info("Overriding ${propertiesFileName} with System properties: graphdb.sparql.url: {}", this)
+                    properties.setProperty("graphdb.sparql.url", this)
+                }
+            }
+
+            log.info("Loaded ${propertiesFileName}: graphdb.sparql.url: {}", properties.get("graphdb.sparql.url"))
+            properties
+        }
+    }
+
+    private fun getInputStreamFromClassPathResource(filename: String): InputStream? {
+        val file = Paths.get(filename)
+        if (Files.exists(file)) {
+            log.info("Using file: {}", file.toAbsolutePath())
+            return Files.newInputStream(file)
+        }
+        log.info("Using classpath resource: {}", filename)
+        return Thread.currentThread().contextClassLoader.getResourceAsStream(filename)
     }
 }
