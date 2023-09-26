28c43e1e48939be373f913cbcab32abdc66230c3
Stephan Oudmaijer <stephan.oudmaijer@tno.nl>
2023-09-26 14:44:57 +0200

    Error handling

--- api/src/main/kotlin/nl/tno/federated/api/event/distribution/rules/BroadcastEventDistributionRule.kt
+++ api/src/main/kotlin/nl/tno/federated/api/event/distribution/rules/BroadcastEventDistributionRule.kt
@@ -1,14 +1,23 @@
 package nl.tno.federated.api.event.distribution.rules
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import nl.tno.federated.api.corda.CordaNodeService
 import nl.tno.federated.api.event.distribution.corda.CordaEventDestination
+import org.slf4j.LoggerFactory
 
 /**
  * This rule refreshes its destinations list based on the latest networkMapSnapshot.
  */
-class BroadcastEventDistributionRule(val cordaNodeService: CordaNodeService) : EventDistributionRule<CordaEventDestination> {
+class BroadcastEventDistributionRule(@JsonIgnore val cordaNodeService: CordaNodeService) : EventDistributionRule<CordaEventDestination> {
 
-    override fun getDestinations() = cordaNodeService.getPeersExcludingSelfAndNotary().map { CordaEventDestination(it.name) }.toSet()
+    private val log = LoggerFactory.getLogger(BroadcastEventDistributionRule::class.java)
+
+    override fun getDestinations() = try {
+        cordaNodeService.getPeersExcludingSelfAndNotary().map { CordaEventDestination(it.name) }.toSet()
+    }catch (e: Exception) {
+        log.error("Failed to retrieve destinations for BroadcastEventDistributionRule: ${e.message}", e)
+        emptySet<CordaEventDestination>()
+    }
 
     override fun appliesTo(ttl: String): Boolean = true
 
