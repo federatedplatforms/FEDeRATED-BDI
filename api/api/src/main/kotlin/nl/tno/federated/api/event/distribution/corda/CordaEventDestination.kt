943913991ee0ae334fa9bb9da4240a9e708d06dc
Stephan Oudmaijer <stephan.oudmaijer@tno.nl>
2024-01-29 09:12:11 +0100

    More idiomatic Kotlin

--- api/api/src/main/kotlin/nl/tno/federated/api/event/distribution/corda/CordaEventDestination.kt
+++ api/api/src/main/kotlin/nl/tno/federated/api/event/distribution/corda/CordaEventDestination.kt
@@ -11,28 +11,31 @@ class CordaEventDestination(destination: CordaX500Name) : EventDestination<Corda
          * EventDestination minimal format: O=Cargobase,L=Dusseldorf,C=DE
          */
         fun parse(eventDestination: String): CordaEventDestination {
-            val result = eventDestination.split(",")
-            val pairs = mutableMapOf<String, String>()
+            val pairs = eventDestination.split(",")
+                .map { it.trim() }
+                .associate { it.split("=").let { keyValue -> keyValue[0].uppercase() to keyValue[1] } }
 
-            for(item in result) {
-                val split = item.split("=")
-                if(split.size == 2) {
-                    pairs[split[0].trim().uppercase()] = split[1].trim()
-                }
+            if (listOf(ORGANISATION, COUNTRY, LOCALITY).any { it !in pairs }) {
+                throw InvalidEventDestinationFormat("Invalid format for: $eventDestination, expected format: O=Cargobase,L=Dusseldorf,C=DE")
             }
 
-            if(!pairs.containsKey(ORGANISATION) || !pairs.containsKey(COUNTRY) || !pairs.containsKey(LOCALITY) ) {
-                throw InvalidEventDestinationFormat("Invalid format for: ${eventDestination}, expected format: O=Cargobase,L=Dusseldorf,C=DE")
-            }
+            val cordaX500Name = CordaX500Name(
+                pairs[COMMON_NAME],
+                pairs[ORGANISATION_UNIT],
+                pairs[ORGANISATION]!!,
+                pairs[LOCALITY]!!,
+                pairs[STATE],
+                pairs[COUNTRY]!!
+            )
 
-            return CordaEventDestination(CordaX500Name(pairs[COMMON_NAME],pairs[ORGANISATION_UNIT],pairs[ORGANISATION]!!, pairs[LOCALITY]!!, pairs[STATE], pairs[COUNTRY]!!))
+            return CordaEventDestination(cordaX500Name)
         }
 
-        const val ORGANISATION = "O"
-        const val ORGANISATION_UNIT = "O"
-        const val LOCALITY = "L"
-        const val STATE = "S"
-        const val COUNTRY = "C"
-        const val COMMON_NAME = "CN"
+        private const val ORGANISATION = "O"
+        private const val ORGANISATION_UNIT = "O"
+        private const val LOCALITY = "L"
+        private const val STATE = "S"
+        private const val COUNTRY = "C"
+        private const val COMMON_NAME = "CN"
     }
 }
