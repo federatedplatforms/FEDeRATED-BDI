a565ab28d71d9996e029ee91dff35e4e20bfabf4
Erik <erik@bloomingdata.com>
2024-03-04 16:38:06 +0100

    Styled menu

--- gui/frontend/src/lib/Menu.svelte
+++ gui/frontend/src/lib/Menu.svelte
@@ -5,11 +5,40 @@
     let number = 192;
 </script>
 
-<label>Company<input type="text" bind:value={company} /></label>
-<label>Year<input type="text" bind:value={year} /></label>
-<label>Number<input type="text" bind:value={number} /></label>
-<button
-    on:click={() => {
-        getEventsFromEndpoint(company, year, number);
-    }}>Load</button
->
+<div class="menu-bar">
+    <label>Company <input type="text" bind:value={company} /></label>
+    <label>Year <input type="text" bind:value={year} /></label>
+    <label>Number <input type="text" bind:value={number} /></label>
+    <button
+        on:click={() => {
+            getEventsFromEndpoint(company, year, number);
+        }}>Load</button
+    >
+</div>
+
+<style>
+    .menu-bar {
+        font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif;
+        font-size: 0.9em;
+        background-color: #007bc6;
+        padding: 8px;
+        color: #ffffff;
+    }
+    .menu-bar input {
+        border-radius: 3px;
+        border: none;
+    }
+    .menu-bar label {
+        margin-left: 10px;
+    }
+    .menu-bar button {
+        background-color: #99cfff;
+        border: none;
+        padding: 5px;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+    .menu-bar button:hover {
+        background-color: #c8e5ff;
+    }
+</style>
