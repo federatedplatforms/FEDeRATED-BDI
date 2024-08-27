e4dd1b334e2e95b0376f2848d343db4b691ee06d
Erwin Somers <erwin.somers@tno.nl>
2024-08-27 13:57:39 +0200

    just house keeping and updated the webhook doc

--- api/api/src/test/kotlin/nl/tno/federated/api/webhook/WebhookTest.kt
+++ api/api/src/test/kotlin/nl/tno/federated/api/webhook/WebhookTest.kt
@@ -11,6 +11,7 @@ class WebhookTest {
 
     private val keyStorePassword = "password"
     private val keyStoreLocation = "node1.p12"
+    private val keyStoreAlias = "node1"
     private val event = ClassPathResource("test-data/MinimalEvent.json").getContentAsString(Charset.defaultCharset())
     private val eventType = "federated.events.minimal.v1"
     private val tokenURL = URL("https://api-test.ntp.gov.sg//oauth/v1/token")
@@ -23,15 +24,38 @@ class WebhookTest {
     fun getPrivateKeyFromKeyStore() {
         val keyStore = KeyStore.getInstance("PKCS12")
         keyStore.load( ClassPathResource(keyStoreLocation).inputStream, keyStorePassword.toCharArray())
-        keyStore.getKey("Node1", "password".toCharArray())
+        keyStore.getKey(keyStoreAlias, keyStorePassword.toCharArray())
+
     }
 
     @Test
     fun testAcquireAccessToken() {
 
         val keyStore = KeyStore.getInstance("PKCS12")
-        keyStore.load( ClassPathResource("node1.p12").inputStream, "password".toCharArray())
-        val privKey = keyStore.getKey("Node1", "password".toCharArray())
+        keyStore.load( ClassPathResource(keyStoreLocation).inputStream, keyStorePassword.toCharArray())
+        val privKey = keyStore.getKey(keyStoreAlias, keyStorePassword.toCharArray())
         webhookHttpClient.getAccessToken(webhook,"NTP",privKey)
     }
+
+    @Test
+    fun registerWebhookWithoutToken () {
+
+    }
+
+    @Test
+    fun registerWebHookWithToken() {
+
+    }
+
+    @Test
+    fun triggerWebhooksWithoutToken() {
+
+    }
+
+    @Test
+    fun triggerWebhooksWithToken() {
+
+    }
+
+
 }
